14aa3b74779c263021cc98234a8f66452880394e
Malachi Danielewicz <mddanielewicz@gmail.com>
2017-05-17 04:10:58 -0400

    Made changes to the catalog pages. Added vars for content. Added classes for style.

--- basic-website/details.php
+++ basic-website/details.php
@@ -28,6 +28,43 @@ include('inc/header.php'); ?>
     <span>
       <img src="<?php echo $item["img"]; ?>" alt="<?php echo $item["title"]; ?>" />
     </span>
-  </div>
 
+    <div class='media-details'>
+      <h1><?php echo $item['title']; ?></h1>
+      <table>
+        <tr>
+          <th>category</th>
+          <td><?php echo $item['category'] ?></td>
+        </tr>
+        <tr>
+          <th>Genre</th>
+          <td><?php echo $item['genre'] ?></td>
+        </tr>
+        <tr>
+          <th>Format</th>
+          <td><?php echo $item['format'] ?></td>
+        </tr>
+        <tr>
+          <th>Year</th>
+          <td><?php echo $item['year'] ?></td>
+        </tr>
+        <?php
+        if (strtolower($item['category']) == 'books') {
+        ?>
+        <tr>
+          <th>Authors</th>
+          <td><?php echo implode($item['authors']); ?></td>
+        </tr>
+        <tr>
+          <th>Publisher</th>
+          <td><?php echo $item['publisher'] ?></td>
+        </tr>
+        <tr>
+          <th>ISBN</th>
+          <td><?php echo $item['isbn'] ?></td>
+        </tr>
+      <?php  } ?>
+      </table>
+    </div>
+  </div>
 </div>
